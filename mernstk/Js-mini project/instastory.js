2ed8d4060b9786b847d974fb7c82c9b53d3d2e2d
Hasnain2525 <h.k789120@gmail.com>
2024-08-09 15:46:16 -0700

    new dom

---
+++ mernstk/Js-mini project/instastory.js
@@ -0,0 +1,42 @@
+let arr = [
+    {
+        dp: "https://images.unsplash.com/photo-1514315384763-ba401779410f?q=80&w=1283&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1529680459049-bf0340fa0755?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1503104834685-7205e8607eb9?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1503001831666-8f3cf3a24544?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1441123694162-e54a981ceba5?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1545912453-3d32e20f72bf?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1610729378720-42d2a7c191fc?q=80&w=1341&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1580008006944-bb3b7e890d5d?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1601288496920-b6154fe3626a?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1607699032287-f58742a2693d?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
+
+let clutter = ""
+arr.forEach((elem,index) => {
+    clutter += ` <div class="story">
+                <img id="${index}" class="story-img" src="${elem.dp}"
+                    alt="">
+            </div>`
+
+})
+let storycrd=document.querySelector(".story-card")
+storycrd.innerHTML=clutter;
+storycrd.addEventListener("click",(dets)=>{
+//   let arr2=arr[dets.target.id].story;
+  document.querySelector(".full-scr").style.display="block";
+  document.querySelector(".full-scr").style.backgroundImage=`url('${arr[dets.target.id].story}')`;
+  setTimeout(() => {
+    document.querySelector(".full-scr").style.display="none";
+  }, 3000);
+
+});
