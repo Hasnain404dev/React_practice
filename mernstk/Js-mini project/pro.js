907841549377a3035b121ad857bc51f0b3ceead9
Hasnain2525 <h.k789120@gmail.com>
2024-08-09 05:52:39 -0700

    upload

--- mernstk/Js-mini project/pro.js
+++ mernstk/Js-mini project/pro.js
@@ -1,5 +1,6 @@
-let fs  =document.querySelector(".img-dis2")
-let btn=document.getElementById("btn1")
+//   1.Add friend Feature with same button
+// let fs  =document.querySelector(".img-dis2")
+// let btn=document.getElementById("btn1")
 // btn.addEventListener("click",function(){
 //     fs.innerHTML="Friend"
 //     fs.style.color="green"
@@ -10,19 +11,44 @@ let btn=document.getElementById("btn1")
 //     fs.style.color="red"
 // })
 // second method
-let click=0;
-btn.addEventListener("click",()=>{
-    if (click==0) {
-        fs.innerHTML="Friend"
-        fs.style.color="green"
-        btn.innerHTML="Remove Friend"
-        btn.style.backgroundColor="lightgrey"
-        click=1;
-    } else {
-        fs.innerHTML="Stranger"
-        fs.style.color="red"
-        btn.innerHTML="Add Friend"
-        btn.style.backgroundColor="Green"
-        click=0
-    }
+// let click=0;
+// btn.addEventListener("click",()=>{
+//     if (click==0) {
+//         fs.innerHTML="Friend"
+//         fs.style.color="green"
+//         btn.innerHTML="Remove Friend"
+//         btn.style.backgroundColor="lightgrey"
+//         click=1;
+//     } else {
+//         fs.innerHTML="Stranger"
+//         fs.style.color="red"
+//         btn.innerHTML="Add Friend"
+//         btn.style.backgroundColor="Green"
+//         click=0
+//     }
+// })
+
+// 2.Insta Love btn
+
+// let cont=document.querySelector(".main")
+// let dclick=document.querySelector(".img-1");
+// dclick.addEventListener("dblclick",()=>{
+//    cont.style.transform="translate(-50%,-50%) scale(1)"
+//    cont.style.opacity="0.8"
+//     setTimeout(() => {
+//          cont.style.transform="translate(-50%,-50%) scale(0)"
+//            cont.style.opacity="0"
+//     },2000 );
+// })
+// 3. Custom cursor
+let main= document.querySelector(".main")
+// console.log(main)
+let crsr=document.querySelector(".cursor")
+
+main.addEventListener("mousemove",(dets)=>{
+    crsr.style.top=dets.y +"px"
+    crsr.style.left=dets.x +"px"
+  
+    // console.log(dets.x)
+
 })
